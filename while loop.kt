29a896ea0c438ed2dd2db38fdf96a0cf36e01601
sunilsahu000 <72176852+sunilsahu000@users.noreply.github.com>
2020-10-01 09:03:48 +0530

    Update while loop.kt
    I have made for loop in step 2-2 in your code and I have used curly bracket in string interpolation but it does not matter.
    Thank you..

--- while loop.kt
+++ while loop.kt
@@ -1,17 +1,33 @@
+
 fun main(args:Array<String>)
 {
     var count=1
-    while(count<=5)
+    while(count<5)
     {
-    for (item in 1..5)
+    for (item in 1..100 step 5)
     {
-        if (item == 2)
+        if (true)
         {
-            println("Count: $item")
+            println("Count: ${item}")
         }
         count++
     }
-    println("Loop done")
+    println("while Loop done")
 
     }
 }
+  
+   
+    
+    
+  
+    
+       
+        
+          
+        
+       
+    
+
+
+
